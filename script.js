e0b7f62f168de45425edc3c15595114dddc3efe5
Rana Adnan <160872302+RanaAdnan78@users.noreply.github.com>
2026-06-26 17:25:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let a = 10
+let b = 20
+let tittle = "ReactSikho"
+let isavialable = true
+let Array = [1,4,6,74,6]
+let Task = {
+    id:1 ,
+    name :"Rana Adnan" ,
+    RollNum : 57
+}
+console.log(tittle)
+console.log(a*b)
+console.log(Array[3])
+console.log(`Yr Ye Nahi Ho Skta${Task.name}`)
